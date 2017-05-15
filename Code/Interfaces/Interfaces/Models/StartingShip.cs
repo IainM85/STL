7146cd2e25e6118ec2821de98a5d30b9a26a17df
Iain Andrew Milne <feliix1956@hotmail.com>
2017-05-15 23:55:04 +0100

    piddling about with interfaces and models

---
+++ Code/Interfaces/Interfaces/Models/StartingShip.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class StartingShip : IShip
+    {
+        public StartingShip()
+        {
+            Hardpoints = new List<IHardpoint>();
+        }
+
+
+        public IEnumerable<IHardpoint> Hardpoints { get; set; }
+
+        public decimal Hitpoints { get; set; }
+
+        private string _name = "Enterprise";
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+
+    }
+}
